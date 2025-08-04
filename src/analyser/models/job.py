a6aa74e7e0e59ba514fee0ea347a9b0ca5dbc8d0
k-ccosta <k.accosta98@gmail.com>
2025-08-04 16:47:07 -0400

    feat: add Pydantic models for Job, Resum, Analysis and File

---
+++ src/analyser/models/job.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from typing import List, Any
+
+class Job(BaseModel):
+    id: str
+    name: str
+    main_activities: str
+    prerequisites: str
+    differentials: str
+    sheet_name: str
+    competence: List[Any]
+    strategies: List[Any]
+    qualifications: List[Any]
+    score_competence: List[Any]
---
+++ src/analyser/models/resum.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from typing import List, Any
+
+class Resum(BaseModel):
+    id: str
+    job_id: str
+    content: str
+    opinion: str
+    file: str
+    score_competence: List[Any]
+    score_strategies: List[Any]
+    score_qualifications: List[Any]
